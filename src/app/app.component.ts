b3e6660bae2973e189f99ccee4db86b7fd1b3d93
jules <jules.angkna@gmail.com>
2020-02-10 14:25:59 +0100

    liste de filtre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'movies';
-  public defaultcountry: string = 'us';
+  public defaultcountry: string = 'all';
 
   public movies: any[] = [
     {
       title: 'Joker',
       year: 2019,
-      country: 'us'
+      country: 'us',
+      shown: true
     },
     {
       title: 'Avenger',
       year: 2018,
-      country: 'us'
+      country: 'us',
+      shown: true
     },
     {
       title: 'Parasite',
       year: 2012,
-      country: 'fr'
+      country: 'fr',
+      shown: true
     },
     {
       title: 'Il était une fois dans la creuse...',
       year: 1984,
-      country: 'fr'
+      country: 'fr',
+      shown: true
     }
   ];
 
-  public toggleCountry(): void {
-    if (this.defaultcountry == 'us') {
-      this.defaultcountry = 'fr';
-    } else { 
-      this.defaultcountry ='us';
-    }
+  public toggleAll(): void {
+    this.defaultcountry = 'all';
+    this.movies.forEach((movie: any) => {
+      movie.shown = true;
+    })
+  }
+
+  public toggleUS(): void {
+    this.defaultcountry = 'us';
+    this.movies.forEach((movie: any) => {
+      movie.shown = (movie.country == this.defaultcountry);
+    })
+  }
+
+  public toggleFR(): void {
+    this.defaultcountry = 'fr';
+    this.movies.forEach((movie: any) => {
+      movie.shown = (movie.country == this.defaultcountry);
+    })
+  }
+  
+  public toggleES(): void {
+    this.defaultcountry = 'es';
+    this.movies.forEach((movie: any) => {
+      movie.shown = (movie.country == this.defaultcountry);
+    })
   }
 }
