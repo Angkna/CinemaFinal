2c84665a339cf8887fee00b283ead0f75fcba9d7
jules <jules.angkna@gmail.com>
2020-02-10 11:42:26 +0100

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movies';
+  public title: string = 'movies';
+  public defaultcountry: string = 'us';
+
+  public movies: any[] = [
+    {
+      title: 'Joker',
+      year: 2019,
+      country: 'us'
+    },
+    {
+      title: 'Avenger',
+      year: 2018,
+      country: 'us'
+    },
+    {
+      title: 'Parasite',
+      year: 2012,
+      country: 'fr'
+    }
+  ];
+
+  public toggleCountry(): void {
+    if (this.defaultcountry == 'us') {
+      this.defaultcountry = 'fr';
+    } else { 
+      this.defaultcountry ='us';
+    }
+  }
 }
