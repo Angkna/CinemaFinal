d75b43fe39363e3ed9bae002114662ed44054bd1
jules <jules.angkna@gmail.com>
2020-02-13 14:47:48 +0100

    login form done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { tap, debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup; 
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userTerm: [
+        '', 
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(255)
+        ])
+      ],
+      passwordTerm: [
+        '', 
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(255)
+        ])
+      ]
+    });
+
+    this.loginForm.valueChanges.pipe(
+      debounceTime(400),
+      map(() => {
+        console.log('lancement recherche')
+        this.doLogin();
+      })
+    ).subscribe();
+  }
+
+  public doLogin(): void {
+    
+  }
+
+  public get userTerm(): AbstractControl{
+    return this.loginForm.controls.searchTerm;
+  }
+
+  public get passwordTerm(): AbstractControl{
+    return this.loginForm.controls.searchTerm;
   }
 
 }
